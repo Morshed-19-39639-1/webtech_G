a90e4825ce72e3b98668345bfd9bf8f2ddc6cca7
Morshed-19-39639-1 <78311288+Morshed-19-39639-1@users.noreply.github.com>
2024-08-21 17:38:54 +0600

    Updated

--- Mid project/welcome.php
+++ Mid project/welcome.php
@@ -3,7 +3,7 @@
 session_start();
 echo "Welcome". '  ' . $_SESSION['email']; 
 
-echo" <br> <br>To enroll JOB POINT successfully. <br> ClickMe to  <a href= 'job.php'> ENTER </a> " ;
+echo" <br> <br>To enroll JOB POINT successfully. <br> ClickMe to  <a href= 'job.php'> ENTER  </a> " ;
 
 echo "<a href=' logout.php' > <br> logout </a>";
 
